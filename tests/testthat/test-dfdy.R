83729c13115a076d072e26841e672dcb3d3a3ab5
Bill Denney <wdenney@humanpredictions.com>
2022-01-21 15:23:29 -0500

    Remove rxode2Test from dfdy

--- tests/testthat/test-dfdy.R
+++ tests/testthat/test-dfdy.R
@@ -1,6 +1,5 @@
-rxode2Test(
-  {
-    Vtpol2 <- rxode2("
+test_that("Specified jacobian is captured", {
+  Vtpol2 <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Jacobian
@@ -13,16 +12,15 @@ dy(0) = 0
 ## mu
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ")
+  
+  expect_true(any(rxDfdy(Vtpol2) == "df(y)/dy(dy)"))
+  expect_true(any(rxDfdy(Vtpol2) == "df(dy)/dy(y)"))
+  expect_true(any(rxDfdy(Vtpol2) == "df(dy)/dy(dy)"))
+})
 
-    test_that("Specified jacobian is captured", {
-      expect_true(any(rxDfdy(Vtpol2) == "df(y)/dy(dy)"))
-      expect_true(any(rxDfdy(Vtpol2) == "df(dy)/dy(y)"))
-      expect_true(any(rxDfdy(Vtpol2) == "df(dy)/dy(dy)"))
-    })
+## fixme multiple jacobian definitions raise an error.
 
-    ## fixme multiple jacobian definitions raise an error.
-
-    tmp <- rxode2("
+tmp <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Jacobian
@@ -37,12 +35,12 @@ dy(0) = 0
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ")
 
-    test_that("Doubled jacobain will compile correctly", {
-      expect_equal(class(tmp), "rxode2")
-    })
+test_that("Doubled jacobain will compile correctly", {
+  expect_equal(class(tmp), "rxode2")
+})
 
-    test_that("Jacobian and sensitivity not specified.", {
-      norm <- rxode2("
+test_that("Jacobian and sensitivity not specified.", {
+  norm <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Initial conditions
@@ -51,14 +49,14 @@ dy(0) = 0
 ## mu
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ")
+  
+  expect_false(norm$calcJac)
+  expect_false(norm$calcSens)
+  rxDelete(norm)
+})
 
-      expect_false(norm$calcJac)
-      expect_false(norm$calcSens)
-      rxDelete(norm)
-    })
-
-    test_that("Jacobian specified but sensitivity not specified.", {
-      jac <- rxode2("
+test_that("Jacobian specified but sensitivity not specified.", {
+  jac <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Initial conditions
@@ -67,14 +65,14 @@ dy(0) = 0
 ## mu
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ", calcJac = TRUE)
+  
+  expect_true(jac$calcJac)
+  expect_false(jac$calcSens)
+  rxDelete(jac)
+})
 
-      expect_true(jac$calcJac)
-      expect_false(jac$calcSens)
-      rxDelete(jac)
-    })
-
-    test_that("Sensitivity specified.", {
-      sens <- rxode2("
+test_that("Sensitivity specified.", {
+  sens <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Initial conditions
@@ -83,14 +81,14 @@ dy(0) = 0
 ## mu
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ", calcSens = TRUE)
+  
+  expect_false(sens$calcJac)
+  expect_true(sens$calcSens)
+  rxDelete(sens)
+})
 
-      expect_false(sens$calcJac)
-      expect_true(sens$calcSens)
-      rxDelete(sens)
-    })
-
-    test_that("Jac/Sens can be calculated from \"normal\" model", {
-      norm <- rxode2("
+test_that("Jac/Sens can be calculated from 'normal' model", {
+  norm <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Initial conditions
@@ -99,24 +97,24 @@ dy(0) = 0
 ## mu
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ")
-      jac <- norm
-      jac <- rxode2(jac, calcJac = TRUE)
-      expect_true(jac$calcJac)
-      expect_false(jac$calcSens)
-      sens <- rxode2(jac, calcSens = TRUE)
-      expect_false(sens$calcJac)
-      expect_true(sens$calcSens)
-      full <- rxode2(jac, calcSens = TRUE, calcJac = TRUE)
-      expect_false(sens$calcJac)
-      expect_true(sens$calcSens)
-      rxDelete(jac)
-      rxDelete(sens)
-      rxDelete(norm)
-      rxDelete(full)
-    })
+  jac <- norm
+  jac <- rxode2(jac, calcJac = TRUE)
+  expect_true(jac$calcJac)
+  expect_false(jac$calcSens)
+  sens <- rxode2(jac, calcSens = TRUE)
+  expect_false(sens$calcJac)
+  expect_true(sens$calcSens)
+  full <- rxode2(jac, calcSens = TRUE, calcJac = TRUE)
+  expect_false(sens$calcJac)
+  expect_true(sens$calcSens)
+  rxDelete(jac)
+  rxDelete(sens)
+  rxDelete(norm)
+  rxDelete(full)
+})
 
-    test_that("Jacobian and sensitivity specified.", {
-      sens <- rxode2("
+test_that("Jacobian and sensitivity specified.", {
+  sens <- rxode2("
 d/dt(y)  = dy
 d/dt(dy) = mu*(1-y^2)*dy - y
 ## Initial conditions
@@ -125,56 +123,56 @@ dy(0) = 0
 ## mu
 mu = 1 ## nonstiff; 10 moderately stiff; 1000 stiff
 ", calcSens = TRUE)
+  
+  norm <- rxode2(sens, calcSens = FALSE)
+  expect_false(norm$calcJac)
+  expect_false(norm$calcSens)
+  
+  jac <- rxode2(sens, calcJac = TRUE)
+  expect_true(jac$calcJac)
+  expect_false(jac$calcSens)
+  expect_false(sens$calcJac)
+  expect_true(sens$calcSens)
+})
 
-      norm <- rxode2(sens, calcSens = FALSE)
-      expect_false(norm$calcJac)
-      expect_false(norm$calcSens)
+test_that("Conditional Sensitivites", {
+  transit.if <- rxode2({
+    ## Table 3 from Savic 2007
+    cl <- 17.2 # (L/hr)
+    vc <- 45.1 # L
+    ka1 <- 0.38 # 1/hr
+    ka2 <- 0.2 # 1/hr
+    mtt <- 0.37 # hr
+    bio <- 1
+    n <- 20.1
+    k <- cl / vc
+    if (pop1 == 1) {
+      ka <- ka1
+    } else {
+      ka <- ka2
+    }
+    d / dt(depot) <- transit(n, mtt, bio) - ka * depot
+    d / dt(cen) <- ka * depot - k * cen
+  })
+  
+  expect_false(transit.if$calcJac)
+  expect_false(transit.if$calcSens)
+  
+  jac <- rxode2(transit.if, calcJac = TRUE)
+  expect_true(jac$calcJac)
+  expect_false(jac$calcSens)
+  
+  sens <- rxode2(transit.if, calcSens = TRUE)
+  expect_false(sens$calcJac)
+  expect_true(sens$calcSens)
+  
+  full <- rxode2(transit.if, calcSens = TRUE, calcJac = TRUE)
+  expect_true(full$calcJac)
+  expect_true(full$calcSens)
+})
 
-      jac <- rxode2(sens, calcJac = TRUE)
-      expect_true(jac$calcJac)
-      expect_false(jac$calcSens)
-      expect_false(sens$calcJac)
-      expect_true(sens$calcSens)
-    })
-
-    test_that("Conditional Sensitivites", {
-      transit.if <- rxode2({
-        ## Table 3 from Savic 2007
-        cl <- 17.2 # (L/hr)
-        vc <- 45.1 # L
-        ka1 <- 0.38 # 1/hr
-        ka2 <- 0.2 # 1/hr
-        mtt <- 0.37 # hr
-        bio <- 1
-        n <- 20.1
-        k <- cl / vc
-        if (pop1 == 1) {
-          ka <- ka1
-        } else {
-          ka <- ka2
-        }
-        d / dt(depot) <- transit(n, mtt, bio) - ka * depot
-        d / dt(cen) <- ka * depot - k * cen
-      })
-
-      expect_false(transit.if$calcJac)
-      expect_false(transit.if$calcSens)
-
-      jac <- rxode2(transit.if, calcJac = TRUE)
-      expect_true(jac$calcJac)
-      expect_false(jac$calcSens)
-
-      sens <- rxode2(transit.if, calcSens = TRUE)
-      expect_false(sens$calcJac)
-      expect_true(sens$calcSens)
-
-      full <- rxode2(transit.if, calcSens = TRUE, calcJac = TRUE)
-      expect_true(full$calcJac)
-      expect_true(full$calcSens)
-    })
-
-    test_that("Transit Sensitivities", {
-      mod <- rxode2("
+test_that("Transit Sensitivities", {
+  mod <- rxode2("
 ## Table 3 from Savic 2007
 cl = 17.2 # (L/hr)
 vc = 45.1 # L
@@ -188,52 +186,48 @@ ktr = (n+1)/mtt
 d/dt(depot) = exp(log(bio*podo)+log(ktr)+n*log(ktr*t)-ktr*t-lgammafn(n+1))-ka*depot
 d/dt(cen) = ka*depot-k*cen
 ")
-
-      mod <- rxode2(mod, calcSens = TRUE)
-
-      et <- eventTable()
-      et$add.sampling(seq(0, 10, length.out = 200))
-      et$add.dosing(20, start.time = 0)
-
-      transit <- suppressWarnings({
-        rxSolve(mod, et, transit_abs = TRUE)
-      })
-
-      ## Used the log(0) protection since the depot_mtt sensitivity
-      ## includes log(t*...) and t=0
-      ##
-      ## These results are now system dependent since it uses
-      ## log(sqrt(DOUBLE_EPS)) and DOUBLE_EPS varies by platform, so
-      ## just make sure the results are not NA.
-      expect_true(all(!is.na(transit[["_sens_depot_mtt"]])))
-      expect_equal(transit$depot.mtt, transit[["_sens_depot_mtt"]])
-      expect_equal(transit$depot_mtt, transit[["_sens_depot_mtt"]])
-      expect_equal(transit[["depot_mtt"]], transit[["_sens_depot_mtt"]])
-      expect_equal(transit[["depot.mtt"]], transit[["_sens_depot_mtt"]])
-
-      mod <- rxode2({
-        ## Table 3 from Savic 2007
-        cl <- 17.2 # (L/hr)
-        vc <- 45.1 # L
-        tvka <- 0.38 # 1/hr
-        tvmtt <- 0.37 # hr
-        ka <- tvka * exp(eta_ka)
-        mtt <- tvmtt * exp(eta_mtt)
-        bio <- 1
-        n <- 20.1
-        k <- cl / vc
-        ktr <- (n + 1) / mtt
-        ## note that lgammafn is the same as lgamma in R.
-        d / dt(depot) <- exp(log(bio * podo) + log(ktr) + n * log(ktr * t) - ktr * t - lgammafn(n + 1)) - ka * depot
-        d / dt(cen) <- ka * depot - k * cen
-      })
-
-      tmp <- rxode2(mod, calcSens = c("eta_ka", "eta_mtt"))
-      expect_true(all(!is.na(transit[["_sens_depot_mtt"]])))
-
-      ## tmp <- rxode2(mod, calcSens=list(eta=c("eta_ka", "eta_mtt"), theta=c("cl", "vc")));
-    })
-  },
-  silent = TRUE,
-  test = "parsing"
-)
+  
+  mod <- rxode2(mod, calcSens = TRUE)
+  
+  et <- eventTable()
+  et$add.sampling(seq(0, 10, length.out = 200))
+  et$add.dosing(20, start.time = 0)
+  
+  transit <- suppressWarnings({
+    rxSolve(mod, et, transit_abs = TRUE)
+  })
+  
+  ## Used the log(0) protection since the depot_mtt sensitivity
+  ## includes log(t*...) and t=0
+  ##
+  ## These results are now system dependent since it uses
+  ## log(sqrt(DOUBLE_EPS)) and DOUBLE_EPS varies by platform, so
+  ## just make sure the results are not NA.
+  expect_true(all(!is.na(transit[["_sens_depot_mtt"]])))
+  expect_equal(transit$depot.mtt, transit[["_sens_depot_mtt"]])
+  expect_equal(transit$depot_mtt, transit[["_sens_depot_mtt"]])
+  expect_equal(transit[["depot_mtt"]], transit[["_sens_depot_mtt"]])
+  expect_equal(transit[["depot.mtt"]], transit[["_sens_depot_mtt"]])
+  
+  mod <- rxode2({
+    ## Table 3 from Savic 2007
+    cl <- 17.2 # (L/hr)
+    vc <- 45.1 # L
+    tvka <- 0.38 # 1/hr
+    tvmtt <- 0.37 # hr
+    ka <- tvka * exp(eta_ka)
+    mtt <- tvmtt * exp(eta_mtt)
+    bio <- 1
+    n <- 20.1
+    k <- cl / vc
+    ktr <- (n + 1) / mtt
+    ## note that lgammafn is the same as lgamma in R.
+    d / dt(depot) <- exp(log(bio * podo) + log(ktr) + n * log(ktr * t) - ktr * t - lgammafn(n + 1)) - ka * depot
+    d / dt(cen) <- ka * depot - k * cen
+  })
+  
+  tmp <- rxode2(mod, calcSens = c("eta_ka", "eta_mtt"))
+  expect_true(all(!is.na(transit[["_sens_depot_mtt"]])))
+  
+  ## tmp <- rxode2(mod, calcSens=list(eta=c("eta_ka", "eta_mtt"), theta=c("cl", "vc")));
+})
