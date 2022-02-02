948f3763b78a99082479b9e7f14c3817eae4eeed
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-02 11:09:52 -0600

    Add test for issue 70

---
+++ tests/testthat/test-ui-good.R
@@ -0,0 +1,28 @@
+test_that("model properties after are parsed OK", {
+
+  one.compartment <- function() {
+    ini({
+      tka <- 0.45 # Log Ka
+      tcl <- 1 # Log Cl
+      tv <- 3.45    # Log V
+      eta.ka ~ 0.6
+      eta.cl ~ 0.3
+      eta.v ~ 0.1
+      add.sd <- 0.7
+    })
+    model({
+      ka <- exp(tka + eta.ka)
+      cl <- exp(tcl + eta.cl)
+      v <- exp(tv + eta.v)
+      d/dt(depot) = -ka * depot
+      d/dt(center) = ka * depot - cl / v * center
+      cp = center / v
+      cp ~ add(add.sd)
+    })
+    keep = c("WT")
+    drop = c("depot")
+  }
+
+  rxode2(one.compartment)
+
+})
