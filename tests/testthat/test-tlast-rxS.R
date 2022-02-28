4fe825b1c57a82096f93e74aa176cc83ac0f9dbe
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-28 14:12:46 -0600

    Reduce the test text

--- tests/testthat/test-tlast-rxS.R
+++ tests/testthat/test-tlast-rxS.R
@@ -1,3 +1,3 @@
 test_that("rxS for tlast() shouldn't error", {
-  expect_error(rxS("V=exp(lV);\nCL=exp(lCL);\nka=exp(lka);\nktr=exp(lktr);\nn=exp(ln);\nbio=1;\nd/dt(depot)=exp(log(bio*podo)+log(ktr)+n*log(ktr*tad(depot))-ktr*tad(depot)-lgamma(n+1))-ka*depot;\nd/dt(central)=ka*depot-CL/V*central;\ncentral_conc=central/V;\nrx_pred_=central_conc;\n"), NA)
+  expect_error(rxS("d/dt(depot)=exp(log(bio*podo)+log(ktr)+n*log(ktr*tad(depot))-ktr*tad(depot)-lgamma(n+1))-ka*depot"), NA)
 })
