3b39a789bc1f4e283781607fd8faaf5724098696
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-03-01 09:28:40 -0600

    Update spacing for parseFunsDosing.h

--- src/parseFunsDosing.h
+++ src/parseFunsDosing.h
@@ -6,11 +6,11 @@ static inline int handleFunctionDosenum(transFunctions *tf) {
       D_ParseNode *xpn = d_get_child(tf->pn, 2);
       char *v2 = (char*)rc_dup_str(xpn->start_loc.s, xpn->end);
       if (allSpaces(v2)){
-	aAppendN("(double)(_solveData->subjects[_cSub].dosenum)", 45);
-	sAppendN(&sbt, "dosenum()", 9);
+				aAppendN("(double)(_solveData->subjects[_cSub].dosenum)", 45);
+				sAppendN(&sbt, "dosenum()", 9);
       } else {
-	updateSyntaxCol();
-	trans_syntax_error_report_fn(_("'dosenum' does not currently take arguments 'dosenum()'"));
+				updateSyntaxCol();
+				trans_syntax_error_report_fn(_("'dosenum' does not currently take arguments 'dosenum()'"));
       }
     } else {
       updateSyntaxCol();
@@ -30,37 +30,37 @@ static inline int handleFunctionTad(transFunctions *tf) {
       D_ParseNode *xpn = d_get_child(tf->pn, 2);
       char *v2 = (char*)rc_dup_str(xpn->start_loc.s, xpn->end);
       if (allSpaces(v2)){
-	// tad overall
-	sAppend(&sb, "_%s0()", tf->v);
-	sAppend(&sbDt, "_%s0()", tf->v);
+				// tad overall
+				sAppend(&sb, "_%s0()", tf->v);
+				sAppend(&sbDt, "_%s0()", tf->v);
       } else {
-	sAppend(&sb, "_%s1(", tf->v);
-	sAppend(&sbDt, "_%s1(", tf->v);
-	if (new_de(v2)){
-	  if (!strcmp("depot", v2)){
-	    tb.hasDepot = 1;
-	    aAppendN("_DEPOT_)", 8);
-	  } else if (!strcmp("central", v2)){
-	    tb.hasCentral = 1;
-	    aAppendN("_CENTRAL_)", 10);
-	  } else if (rx_syntax_require_ode_first){
-	    updateSyntaxCol();
-	    sPrint(&_gbuf,ODEFIRST,v2);
-	    trans_syntax_error_report_fn(_gbuf.s);
-	    /* Free(v2); */
-	    /* Free(tf->v); */
-	    return 1;
-	  } else {
-	    tb.statei++;
-	    sAppend(&sb, "%d)", tb.de.n);
-	    sAppend(&sbDt, "%d)", tb.de.n);
-	  }
-	} else {
-	  new_or_ith(v2);
-	  sAppend(&sb, "%d)", tb.id);
-	  sAppend(&sbDt, "%d)", tb.id);
-	}
-	// tad(cmt)
+				sAppend(&sb, "_%s1(", tf->v);
+				sAppend(&sbDt, "_%s1(", tf->v);
+				if (new_de(v2)){
+					if (!strcmp("depot", v2)){
+						tb.hasDepot = 1;
+						aAppendN("_DEPOT_)", 8);
+					} else if (!strcmp("central", v2)){
+						tb.hasCentral = 1;
+						aAppendN("_CENTRAL_)", 10);
+					} else if (rx_syntax_require_ode_first){
+						updateSyntaxCol();
+						sPrint(&_gbuf,ODEFIRST,v2);
+						trans_syntax_error_report_fn(_gbuf.s);
+						/* Free(v2); */
+						/* Free(tf->v); */
+						return 1;
+					} else {
+						tb.statei++;
+						sAppend(&sb, "%d)", tb.de.n);
+						sAppend(&sbDt, "%d)", tb.de.n);
+					}
+				} else {
+					new_or_ith(v2);
+					sAppend(&sb, "%d)", tb.id);
+					sAppend(&sbDt, "%d)", tb.id);
+				}
+				// tad(cmt)
       }
       sAppend(&sbt, "%s(%s)", tf->v, v2);
       tf->i[0] = tf->nch;
