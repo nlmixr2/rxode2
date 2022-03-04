8fe899a3f996fbaf3067b4cb1accc00feb06c9f6
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-03-04 13:40:36 -0600

    CF for parse tad

--- src/parseFunsDosing.h
+++ src/parseFunsDosing.h
@@ -22,53 +22,65 @@ static inline int handleFunctionDosenum(transFunctions *tf) {
   return 0;
 }
 
+static inline int isFunctionTadType(transFunctions *tf) {
+	(tf->isTad = !strcmp("tad", tf->v)) || (tf->isTafd = !strcmp("tafd", tf->v)) ||
+		(tf->isTlast = !strcmp("tlast", tf->v)) || (tf->isTfirst = !strcmp("tfirst", tf->v)) ||
+		(tf->isDose = !strcmp("dose", tf->v)) ||
+		(tf->isPodo = !strcmp("podo", tf->v));
+}
+static inline int handleFunctionTadEmptyCcode(transFunctions *tf,char *v2) {
+	if (allSpaces(v2)){
+		// tad overall
+		if (tf->isPodo) {
+			sAppend(&sb, "_%s1(%d)", tf->v, tb.lastDdt);
+			sAppend(&sbDt, "_%s1(%d)", tf->v, tb.lastDdt);
+		} else {
+			sAppend(&sb, "_%s0()", tf->v);
+			sAppend(&sbDt, "_%s0()", tf->v);
+		}
+		return 1;
+	} 
+	return 0;
+}
+
+static inline int handleFunctionTadSingleStateCcode(transFunctions *tf,char *v2) {
+	sAppend(&sb, "_%s1(", tf->v);
+	sAppend(&sbDt, "_%s1(", tf->v);
+	if (new_de(v2)){
+		if (!strcmp("depot", v2)){
+			tb.hasDepot = 1;
+			aAppendN("_DEPOT_)", 8);
+		} else if (!strcmp("central", v2)){
+			tb.hasCentral = 1;
+			aAppendN("_CENTRAL_)", 10);
+		} else if (rx_syntax_require_ode_first){
+			updateSyntaxCol();
+			sPrint(&_gbuf,ODEFIRST,v2);
+			trans_syntax_error_report_fn(_gbuf.s);
+			/* Free(v2); */
+			/* Free(tf->v); */
+			return 1;
+		} else {
+			tb.statei++;
+			sAppend(&sb, "%d)", tb.de.n);
+			sAppend(&sbDt, "%d)", tb.de.n);
+		}
+	} else {
+		new_or_ith(v2);
+		sAppend(&sb, "%d)", tb.id);
+		sAppend(&sbDt, "%d)", tb.id);
+	}
+	// tad(cmt)
+}
+
 static inline int handleFunctionTad(transFunctions *tf) {
-  if ((tf->isTad = !strcmp("tad", tf->v)) || (tf->isTafd = !strcmp("tafd", tf->v)) ||
-      (tf->isTlast = !strcmp("tlast", tf->v)) || (tf->isTfirst = !strcmp("tfirst", tf->v)) ||
-			(tf->isDose = !strcmp("dose", tf->v)) ||
-			(tf->isPodo = !strcmp("podo", tf->v))) {
+  if (isFunctionTadType(tf)) {
     int ii = d_get_number_of_children(d_get_child(tf->pn,3))+1;
-    if (ii == 1){
+    if (ii == 1) {
       D_ParseNode *xpn = d_get_child(tf->pn, 2);
       char *v2 = (char*)rc_dup_str(xpn->start_loc.s, xpn->end);
-      if (allSpaces(v2)){
-				// tad overall
-				if (tf->isPodo) {
-					sAppend(&sb, "_%s1(%d)", tf->v, tb.lastDdt);
-					sAppend(&sbDt, "_%s1(%d)", tf->v, tb.lastDdt);
-				} else {
-					sAppend(&sb, "_%s0()", tf->v);
-					sAppend(&sbDt, "_%s0()", tf->v);
-				}
-      } else {
-				sAppend(&sb, "_%s1(", tf->v);
-				sAppend(&sbDt, "_%s1(", tf->v);
-				if (new_de(v2)){
-					if (!strcmp("depot", v2)){
-						tb.hasDepot = 1;
-						aAppendN("_DEPOT_)", 8);
-					} else if (!strcmp("central", v2)){
-						tb.hasCentral = 1;
-						aAppendN("_CENTRAL_)", 10);
-					} else if (rx_syntax_require_ode_first){
-						updateSyntaxCol();
-						sPrint(&_gbuf,ODEFIRST,v2);
-						trans_syntax_error_report_fn(_gbuf.s);
-						/* Free(v2); */
-						/* Free(tf->v); */
-						return 1;
-					} else {
-						tb.statei++;
-						sAppend(&sb, "%d)", tb.de.n);
-						sAppend(&sbDt, "%d)", tb.de.n);
-					}
-				} else {
-					new_or_ith(v2);
-					sAppend(&sb, "%d)", tb.id);
-					sAppend(&sbDt, "%d)", tb.id);
-				}
-				// tad(cmt)
-      }
+			handleFunctionTadEmptyCcode(tf, v2) ||
+				handleFunctionTadSingleStateCcode(tf, v2);
       sAppend(&sbt, "%s(%s)", tf->v, v2);
       tf->i[0] = tf->nch;
       return 1;
