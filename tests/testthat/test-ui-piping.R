773e50793a6aa1fbf3f5416f5fcd1bdc456f0b1a
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-03-18 22:33:09 -0500

    CF fix

--- tests/testthat/test-ui-piping.R
+++ tests/testthat/test-ui-piping.R
@@ -1276,7 +1276,7 @@ test_that("Add covariate to model works", {
       tcl <- exp(1) # Cl
       ## This works with interactive models
       ## You may also label the preceding line with label("label text")
-      tv <- exp(3.45); # log V
+      tv <- exp(3.45) # log V
       ## the label("Label name") works with all models
       add.sd <- 0.7
     })
