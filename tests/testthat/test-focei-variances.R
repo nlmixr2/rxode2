d9bf3b7a40994f5338c173f153ac1866cb431561
Bill Denney <wdenney@humanpredictions.com>
2022-03-23 10:03:03 -0400

    silence tests

--- tests/testthat/test-focei-variances.R
+++ tests/testthat/test-focei-variances.R
@@ -25,15 +25,15 @@ test_that("constant error model tests", {
   # Var = a^2
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd))
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd)))
 
   expect_equal(v1(tmp1),
                quote((add.sd) ^ 2))
 
   tmp2 <-
-    f %>%
-    model(ipre ~ add(f2))
+    suppressMessages(f %>%
+    model(ipre ~ add(f2)))
 
 
   expect_equal(v1(tmp2),
@@ -45,43 +45,43 @@ test_that("prop model tests", {
   # Var = (f*b)^2
 
   tmp1 <-
-    f %>%
-    model(ipre ~ prop(prop.sd))
+    suppressMessages(f %>%
+    model(ipre ~ prop(prop.sd)))
 
   expect_equal(v1(tmp1),
                quote((rx_pred_f_ * prop.sd) ^ 2))
 
   tmp2 <-
-    f %>%
-    model(ipre ~ prop(f2))
+    suppressMessages(f %>%
+    model(ipre ~ prop(f2)))
 
   expect_equal(v1(tmp2),
                quote((rx_pred_f_ * f2) ^ 2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ propT(prop.sd))
+    suppressMessages(f %>%
+    model(ipre ~ propT(prop.sd)))
 
   expect_equal(v1(tmp1),
                quote((rx_pred_ * prop.sd) ^ 2))
 
   tmp2 <-
-    f %>%
-    model(ipre ~ propT(f2))
+    suppressMessages(f %>%
+    model(ipre ~ propT(f2)))
 
   expect_equal(v1(tmp2),
                quote((rx_pred_ * f2) ^ 2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ propF(prop.sd, f3))
+    suppressMessages(f %>%
+    model(ipre ~ propF(prop.sd, f3)))
 
   expect_equal(v1(tmp1),
                quote((f3 * prop.sd) ^ 2))
 
   tmp2 <-
-    f %>%
-    model(ipre ~ propF(f2, f3))
+    suppressMessages(f %>%
+    model(ipre ~ propF(f2, f3)))
 
   expect_equal(v1(tmp2),
                quote((f3 * f2) ^ 2))
@@ -93,52 +93,52 @@ test_that("power models", {
 
   # pow(sd,pw)
   tmp1 <-
-    f %>%
-    model(ipre ~ pow(prop.sd, pw))
+    suppressMessages(f %>%
+    model(ipre ~ pow(prop.sd, pw)))
 
   expect_equal(v1(tmp1),
                quote(((rx_pred_f_)^(pw) * prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ pow(f2, pw))
+    suppressMessages(f %>%
+    model(ipre ~ pow(f2, pw)))
 
   expect_equal(v1(tmp1),
                quote(((rx_pred_f_)^(pw) * f2)^2))
 
   # powT(sd,pw)
   tmp1 <-
-    f %>%
-    model(ipre ~ powT(prop.sd, pw))
+    suppressMessages(f %>%
+    model(ipre ~ powT(prop.sd, pw)))
 
   expect_equal(v1(tmp1),
                quote(((rx_pred_)^(pw) * prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ powT(f2, pw))
+    suppressMessages(f %>%
+    model(ipre ~ powT(f2, pw)))
 
   expect_equal(v1(tmp1),
                quote(((rx_pred_)^(pw) * f2)^2))
 
   # powF(sd,pw,f)
   tmp1 <-
-    f %>%
-    model(ipre ~ powF(prop.sd, pw, f3))
+    suppressMessages(f %>%
+    model(ipre ~ powF(prop.sd, pw, f3)))
 
   expect_equal(v1(tmp1),
                quote(((f3)^(pw) * prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ powF(f2, pw, f3))
+    suppressMessages(f %>%
+    model(ipre ~ powF(f2, pw, f3)))
 
   expect_equal(v1(tmp1),
                quote(((f3)^(pw) * f2)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ powF(f2, lipre, f3))
+    suppressMessages(f %>%
+    model(ipre ~ powF(f2, lipre, f3)))
 
   expect_equal(v1(tmp1),
                quote(((f3)^(lipre) * f2)^2))
@@ -149,87 +149,87 @@ test_that("add+prop combined1", {
   # Var = (a + b*f^c)^2
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + prop(prop.sd) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + prop(prop.sd) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_f_) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + prop(prop.sd) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + prop(prop.sd) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_f_) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + prop(f2) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + prop(f2) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_f_) * (f2))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + prop(f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + prop(f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_f_) * (f3))^2))
 
   # propT
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propT(prop.sd) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propT(prop.sd) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + propT(prop.sd) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + propT(prop.sd) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propT(f2) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propT(f2) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_) * (f2))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + propT(f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + propT(f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_) * (f3))^2))
 
   # propF
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propF(prop.sd, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propF(prop.sd, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (f3) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + propF(prop.sd, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + propF(prop.sd, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((lipre) + (f3) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propF(lipre, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propF(lipre, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (f3) * (lipre))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + propF(f2, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + propF(f2, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((lipre) + (f3) * (f2))^2))
@@ -241,88 +241,88 @@ test_that("add+pow combined1", {
   # Var = (a + b*f^c)^2
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + pow(prop.sd, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + pow(prop.sd, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_f_) ^ (c) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + pow(prop.sd, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + pow(prop.sd, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_f_) ^ (c) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + pow(f2, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + pow(f2, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_f_) ^ (c)* (f2))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + pow(f3, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + pow(f3, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_f_) ^ (c) * (f3))^2))
 
   # propT
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powT(prop.sd, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powT(prop.sd, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_) ^ (c) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + powT(prop.sd, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + powT(prop.sd, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_) ^ (c)* (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powT(f2, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powT(f2, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (rx_pred_) ^ (c)* (f2))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + powT(f3, c) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + powT(f3, c) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((f2) + (rx_pred_) ^ (c) * (f3))^2))
 
   # propF
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powF(prop.sd, c, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powF(prop.sd, c, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (f3) ^ (c) * (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + powF(prop.sd, c, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + powF(prop.sd, c, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((lipre) + (f3) ^ (c)* (prop.sd))^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powF(lipre, c, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powF(lipre, c, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((add.sd) + (f3) ^ (c) * (lipre))^2))
 
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + powF(f2, c, f3) + combined1())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + powF(f2, c, f3) + combined1()))
 
   expect_equal(v1(tmp1),
                quote(((lipre) + (f3) ^ (c)* (f2))^2))
@@ -334,88 +334,88 @@ test_that("add+prop combined2", {
   # Var = a^2 + b^2*(f^c)^2
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + prop(prop.sd) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + prop(prop.sd) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + (rx_pred_f_)^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + prop(prop.sd) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + prop(prop.sd) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + (rx_pred_f_)^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + prop(f2) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + prop(f2) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + (rx_pred_f_)^2 * (f2)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + prop(f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + prop(f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + (rx_pred_f_)^2 * (f3)^2))
 
   # propT
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propT(prop.sd) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propT(prop.sd) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + (rx_pred_)^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + propT(prop.sd) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + propT(prop.sd) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + (rx_pred_)^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propT(f2) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propT(f2) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + (rx_pred_)^2 * (f2)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + propT(f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + propT(f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + (rx_pred_)^2 * (f3)^2))
 
   # propF
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propF(prop.sd, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propF(prop.sd, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + (f3)^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + propF(prop.sd, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + propF(prop.sd, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((lipre)^2 + (f3)^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + propF(lipre, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + propF(lipre, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + (f3)^2 * (lipre)^2))
 
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + propF(f2, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + propF(f2, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((lipre)^2 + (f3)^2 * (f2)^2))
@@ -427,87 +427,87 @@ test_that("add+pow combined2", {
   # Var = (a + b*f^c)^2
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + pow(prop.sd, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + pow(prop.sd, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + ((rx_pred_f_)^(c))^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + pow(prop.sd, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + pow(prop.sd, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + ((rx_pred_f_)^(c))^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + pow(f2, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + pow(f2, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + ((rx_pred_f_)^(c))^2 * (f2)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + pow(f3, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + pow(f3, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + ((rx_pred_f_)^(c))^2 * (f3)^2))
 
   # propT
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powT(prop.sd, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powT(prop.sd, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + ((rx_pred_)^(c))^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + powT(prop.sd, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + powT(prop.sd, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + ((rx_pred_)^(c))^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powT(f2, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powT(f2, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + ((rx_pred_)^(c))^2 * (f2)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(f2) + powT(f3, c) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(f2) + powT(f3, c) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((f2)^2 + ((rx_pred_)^(c))^2 * (f3)^2))
 
   # propF
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powF(prop.sd, c, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powF(prop.sd, c, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + ((f3)^(c))^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + powF(prop.sd, c, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + powF(prop.sd, c, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((lipre)^2 + ((f3)^(c))^2 * (prop.sd)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(add.sd) + powF(lipre, c, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(add.sd) + powF(lipre, c, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((add.sd)^2 + ((f3)^(c))^2 * (lipre)^2))
 
   tmp1 <-
-    f %>%
-    model(ipre ~ add(lipre) + powF(f2, c, f3) + combined2())
+    suppressMessages(f %>%
+    model(ipre ~ add(lipre) + powF(f2, c, f3) + combined2()))
 
   expect_equal(v1(tmp1),
                quote((lipre)^2 + ((f3)^(c))^2 * (f2)^2))
