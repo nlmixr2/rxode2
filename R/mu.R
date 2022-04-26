e84c71f16984e3fc84b46cc1d6dd9f2f73d0928f
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-04-25 21:28:52 -0500

    Reset for left handed side of equals and right handed side of equals

--- R/mu.R
+++ R/mu.R
@@ -619,9 +619,14 @@
           # separated into mu-referenced line
           .rxMuRefLineIsClean(x, env)
           if (rxode2.debug) {
-            .tmp <- lapply(x, .rxMuRef0, env=env)
+            .tmp <- lapply(x, function(y, env){
+              assign(".curEval", "", env)
+              .rxMuRef0(y, env=env)
+            }, env=env)
           } else {
-            .tmp <- try(lapply(x, .rxMuRef0, env=env), silent=TRUE)
+            .tmp <- try(lapply(x, function(y, env){
+              .rxMuRef0(y, env=env)
+            }, env=env), silent=TRUE)
           }
           if (inherits(.tmp, "try-error")) {
             .msg <- paste0("mu-ref err: ", attr(.tmp,"condition")$message)
@@ -643,7 +648,7 @@
     }
   } else if (is.call(x)) {
     if (identical(x[[1]], quote(`+`))) {
-    .muRefHandlePlus(x, env)
+      .muRefHandlePlus(x, env)
     } else {
       assign(".curEval", as.character(x[[1]]), env)
       .handleSingleEtaIfExists(x[[2]], env)
