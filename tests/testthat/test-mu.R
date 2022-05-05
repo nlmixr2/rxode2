32741cd070ccd52cc1c813558ede28715dd45142
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-05 17:51:13 -0500

    Test the mu referencing degradation

--- tests/testthat/test-mu.R
+++ tests/testthat/test-mu.R
@@ -593,4 +593,32 @@ rxTest({
     expect_equal(curEval$curEval[w], "")
 
   })
+
+  test_that("test mu-reference covariate degradation", {
+
+    one.cmt <- function() {
+      ini({
+        tka <- 0.45 ; label("Ka")
+        tcl <- log(c(0, 2.7, 100)) ; label("Log Cl")
+        tv <- 3.45; label("log V")
+        cl.wt <- 0
+        v.wt <- 0
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl + WT * cl.wt)
+        v <- exp(tv + eta.v) + WT ^ 2 * v.wt
+        linCmt() ~ add(add.sd)
+      })
+    }
+
+    ui <- rxode2::rxode(one.cmt)
+
+    expect_equal(length(ui$muRefCovariateDataFrame$theta), 0)
+
+  })
 })
