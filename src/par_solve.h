7fc95f68821841a503f0b0f966883e9423f6413d
matt <matthew.fidler@gmail.com>
2022-07-26 19:51:19 -0500

    initialize whole of the llik save space

--- src/par_solve.h
+++ src/par_solve.h
@@ -15,7 +15,7 @@ void sortRadix(rx_solving_options_ind *ind);
 static inline int iniSubject(int solveid, int inLhs, rx_solving_options_ind *ind, rx_solving_options *op, rx_solve *rx,
                              t_update_inis u_inis) {
 	setIndPointersByThread(ind);
-	for (int i=9; i--;) {
+	for (int i=rxLlikSaveSize*op->nLlik; i--;) {
 		ind->llikSave[i] = 0.0;
 	}
   ind->ixds = ind->idx = ind->_update_par_ptr_in = 0; // reset dosing
