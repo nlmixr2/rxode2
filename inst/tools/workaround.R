ad8261f9d5eb5158ed824e77e6bc075f85c8e7c7
matt <matthew.fidler@gmail.com>
2022-09-26 19:28:45 -0500

    Add compiler flags

--- inst/tools/workaround.R
+++ inst/tools/workaround.R
@@ -64,10 +64,25 @@ unlink("R/rxode2_md5.R")
 cpp <- list.files("src", pattern = ".(c|h|cpp|f)$")
 include <- list.files("inst/include")
 #Rfiles <- list.files("R/", pattern = ".R")
-md5 <- digest::digest(lapply(c(paste0("src/", cpp),
-                               paste0("inst/include/", include)#,
-                               #paste0("R/", Rfiles)
-                               ), digest::digest, file = TRUE))
+
+cmd <- file.path(R.home("bin"), "R")
+args <- c("CMD", "config")
+
+md5 <- digest::digest(c(lapply(c(paste0("src/", cpp),
+                                 paste0("inst/include/", include)#,
+                                 #paste0("R/", Rfiles)
+                                 ), digest::digest, file = TRUE),
+                        vapply(c("BLAS_LIBS", "CC",  "CFLAGS", "CPICFLAGS",
+                                 "CXX", "CXXFLAGS", "CXXPICFLAGS",
+                                 "CXX11", "CXX11STD", "CXX11FLAGS", "CXX11PICFLAGS",
+                                 "CXX14", "CXX14STD", "CXX14FLAGS", "CXX14PICFLAGS",
+                                 "CXX17", "CXX17STD", "CXX17FLAGS", "CXX17PICFLAGS",
+                                 "CXX20", "CXX20STD", "CXX20FLAGS", "CXX20PICFLAGS",
+                                 "FC", "FFLAGS", "FCFLAGS",  "FPICFLAGS"),
+                               function(cfg) {
+                                 rawToChar(sys::exec_internal(cmd, c(args, cfg))$stdout)
+                               }, character(1))
+                        ))
 unlink("R/rxode2_md5.R")
 md5file <- file("R/rxode2_md5.R", "wb")
 writeLines(sprintf("rxode2.md5 <- \"%s\"\n", md5), md5file)
