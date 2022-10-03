3204422fec155df84fa4f9e992b8d7ff4f2d2175
matt <matthew.fidler@gmail.com>
2022-10-03 13:26:05 -0500

    Replace rxode2parseVer.h

--- src/rxode2_df.cpp
+++ src/rxode2_df.cpp
@@ -27,7 +27,7 @@
 #include "checkmate.h"
 #include <stdint.h>    // for uint64_t rather than unsigned long long
 #include "../inst/include/rxode2.h"
-#include "ode.h"
+#include <rxode2parseVer.h>
 #include "handle_evid.h"
 #include "getTime.h"
 #include "par_solve.h"
