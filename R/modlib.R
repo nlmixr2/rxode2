882f9ac69c0a898e93d30c746e5396b091773fae
matt <matthew.fidler@gmail.com>
2022-10-08 21:00:54 -0500

    Fix rxUse

--- R/modlib.R
+++ R/modlib.R
@@ -346,6 +346,8 @@ rxPkg <- function(..., package,
   )
   setwd(.dir2)
   usethis::use_package("rxode2", "LinkingTo")
+  usethis::use_package("rxode2random", "LinkingTo")
+  usethis::use_package("rxode2parse", "LinkingTo")
   usethis::use_package("rxode2", "Depends")
   if (license == "gpl3") {
     usethis::use_gpl3_license()
