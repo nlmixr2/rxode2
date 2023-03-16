4ebab0fb24edd48705be46ca4fba499b442ebe47
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-03-15 21:32:43 -0500

    CF fixes

--- tests/testthat/test-ui-assign-model-parts.R
+++ tests/testthat/test-ui-assign-model-parts.R
@@ -83,7 +83,9 @@ test_that("rxode2<- and other rxUi methods", {
   rxode2(fun1) <- body(fun2)
   expect_equal(deparse1(fun1), deparse1(fun2))
 
-  expect_error({rxode2(fun1) <- "matt"})
+  expect_error({
+    rxode2(fun1) <- "matt"
+  })
 
   uiOne <- rxode2(one.compartment)
   uiTwo <- uiOne
