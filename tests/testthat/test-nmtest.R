bd44151194ad3b32b63382ee7936b6578bca970b
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-04-11 17:29:04 -0500

    Revert "Capture rx->op->nCalcInf"
    This reverts commit da4b9ca9d7c6b1b591761ecb38b1c941ba1de52b.

--- tests/testthat/test-nmtest.R
+++ tests/testthat/test-nmtest.R
@@ -33,9 +33,7 @@ if (file.exists(test_path("test-nmtest.qs"))) {
 
   library(ggplot2)
 
-  p <- TRUE
-
-  solveEqual <- function(id, plot = p) {
+  solveEqual <- function(id, plot = FALSE) {
     noLag <-  d[d$id == id & d$evid != 0,]$lagt == 0
     if (plot) {
       print(etTrans(d[d$id == id,], fl))
@@ -56,20 +54,6 @@ if (file.exists(test_path("test-nmtest.qs"))) {
     }
   }
 
-  # with id 10, dur=50 which is greater than ii
-  # with id 11, dur=50 which is grater than ii.
-  # with id 12, dur=12, which is greater than ii
-  # with id 13, dur=10 which equals ii
-  # with id 14, dur=10=ii
-  # 25 ss2 not working like nonmem; it seems that the addl ss=1 accumulates something...
-
-  # 19 need to work on modeled rates/durations
-
-  #invisible(lapply(seq_len(range(d$id)[2]), solveEqual))
-
-  # for NONMEM add case for 25 where addl isn't present
-  # Also add 2 more cases for 25 using infusions
-  
-  # for 9 add case where lag time is longer so that it goes through the infusion time
+  invisible(lapply(seq_len(range(d$id)[2]), solveEqual))
 
 }
