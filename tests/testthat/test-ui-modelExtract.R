d3dfaa178e77410ee96d746d221ca2b2261732e3
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-06-21 17:56:39 -0500

    Add test for multiple lines

--- tests/testthat/test-ui-modelExtract.R
+++ tests/testthat/test-ui-modelExtract.R
@@ -38,4 +38,29 @@ test_that("modelExtract and related functions", {
   expect_equal(modelExtract(f, "cp", expression=FALSE, endpoint=FALSE),
                "cp = center/v")
 
+  one.compartment <- function() {
+    ini({
+      tka <- 0.45 # Log Ka
+      tcl <- 1 # Log Cl
+      tv <- 3.45    # Log V
+      eta.ka ~ 0.6
+      eta.cl ~ 0.3
+      eta.v ~ 0.1
+      add.sd <- 0.7
+    })
+    model({
+      ka <- exp(tka + eta.ka)
+      cl <- tcl
+      cl <- cl*exp(eta.cl)
+      v <- exp(tv + eta.v)
+      d/dt(depot) = -ka * depot
+      d/dt(center) = ka * depot - cl / v * center
+      cp = center / v
+      cp ~ add(add.sd)
+    })
+  }
+  
+  expect_equal(modelExtract(one.compartment, "cl", expression=FALSE, endpoint=FALSE),
+               c("cl <- tcl", "cl <- cl * exp(eta.cl)"))
+
 })
