9bc8275b8d955bbf1fe269dd321af86f0eb1b261
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-08-17 14:42:02 -0500

    Remove rxSolve info

--- tests/testthat/test-as-nonmem2rx-solve.R
+++
@@ -1,7 +0,0 @@
-if (file.exists(test_path("test-nmtest.qs"))) {
-  test_that("test-as-nonmem2rx-solve", {
-    d <- qs::qread(test_path("test-as-nonmem2rx-solve.qs"))
-    d[[1]] <- rxode2(d[[1]])
-    expect_error(do.call(rxSolve, d), NA)
-  })
-}
