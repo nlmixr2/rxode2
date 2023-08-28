e59ae271dcaf4447ae8f4664f39a554bd69891d7
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-08-28 18:49:34 -0500

    skip on windows os

--- tests/testthat/test-npde-mac.R
+++ tests/testthat/test-npde-mac.R
@@ -3,7 +3,8 @@ rxTest({
     # This test requires a lot of RAM (approximately 16GiB), so skip it if there
     # is not enough RAM available.  This skip only works on Windows because
     # `memory.limit()` returns Inf (with a warning) on other platforms.
-    skip_if_not(suppressWarnings(memory.limit()) > 16*1024, message="Not enough memory to run test")
+    #skip_if_not(suppressWarnings(memory.limit()) > 16*1024, message="Not enough memory to run test")
+    skip_on_os("windows")
     skip_if_not(file.exists(test_path("si.qs")))
     si <- qs::qread(test_path("si.qs"))
     si$object <- rxode2(si$object)
