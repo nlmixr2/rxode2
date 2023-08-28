28b0051df93dc295b1a3a6508133f4744aa0762f
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-08-28 13:48:15 -0500

    Revert "Remove timeThread"
    This reverts commit 7786478e78502a0b019abb2befa84c9c6aa4a948.

--- src/rxData.cpp
+++ src/rxData.cpp
@@ -1357,6 +1357,9 @@ struct rx_globals {
   double **extraDoseDose    = NULL;
   int     *extraDoseN       = NULL;
   int     *extraDoseAllocN  = NULL;
+
+  // time per thread
+  double *timeThread = NULL;
 };
 
 
@@ -1454,6 +1457,7 @@ extern "C" void setIndPointersByThread(rx_solving_options_ind *ind) {
     ind->solveLast = NULL;
     ind->solveLast2 = NULL;
   }
+  ind->timeThread = _globals.timeThread + rx->maxAllTimes*omp_get_thread_num();
   ind->llikSave = _globals.gLlikSave + op->nLlik*rxLlikSaveSize*omp_get_thread_num();
   ind->lhs = _globals.glhs+op->nlhs*omp_get_thread_num();
 }
@@ -5397,8 +5401,9 @@ SEXP rxSolve_(const RObject &obj, const List &rxControl,
     int n6 = scaleC.size();
     int nIndSim = rx->nIndSim;
     int n7 =  nIndSim * rx->nsub * rx->nsim;
+    int n8 = rx->maxAllTimes*op->cores;
     if (_globals.gsolve != NULL) free(_globals.gsolve);
-    _globals.gsolve = (double*)calloc(n0+nLin+3*nsave+n2+ n4+n5_c+n6+ n7 +
+    _globals.gsolve = (double*)calloc(n0+nLin+3*nsave+n2+ n4+n5_c+n6+ n7 + n8 +
                                       5*op->neq + 8*n3a_c + nllik_c,
                                       sizeof(double));// [n0]
 #ifdef rxSolveT
@@ -5438,6 +5443,7 @@ SEXP rxSolve_(const RObject &obj, const List &rxControl,
     _globals.gTfirstS  = _globals.gTlastS + n3a_c; // [n3a]
     _globals.gCurDoseS = _globals.gTfirstS + n3a_c; // [n3a]
     _globals.gIndSim   = _globals.gCurDoseS + n3a_c;// [n7]
+    _globals.timeThread = _globals.gIndSim + n7;
     std::fill_n(rx->ypNA, op->neq, NA_REAL);
 
     std::fill_n(&_globals.gatol2[0],op->neq, atolNV[0]);
