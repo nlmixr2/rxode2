879f1e619ab00a81da23d42d5e2472016110e46f
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-09-07 15:02:21 -0500

    Align with changes in handleExtraDose

--- src/par_solve.cpp
+++ src/par_solve.cpp
@@ -1132,7 +1132,7 @@ extern "C" void ind_liblsoda0(rx_solve *rx, rx_solving_options *op, struct lsoda
         badSolveExit(i);
       } else {
         // REprintf("xp: %f xout: %f\n", xp, xout);
-        if (handleExtraDose(yp, xout, xp, &i, &(ctx->state), op, ind, u_inis, ctx)) {
+        if (handleExtraDose(yp, xout, xp, &i, op, ind)) {
           if (!isSameTime(ind->extraDoseNewXout, xp)) {
             lsoda(ctx,yp, &xp, ind->extraDoseNewXout);
             postSolve(&(ctx->state), rc, &i, yp, NULL, 0, false, ind, op, rx);
@@ -1510,7 +1510,7 @@ extern "C" void ind_lsoda0(rx_solve *rx, rx_solving_options *op, int solveid, in
         // Bad Solve => NA
         badSolveExit(i);
       } else {
-        if (handleExtraDose(yp, xout, xp, &i, &istate, op, ind, u_inis, ctx)) {
+        if (handleExtraDose(yp, xout, xp, &i, op, ind)) {
           if (!isSameTime(ind->extraDoseNewXout, xp)) {
             F77_CALL(dlsoda)(dydt_lsoda, neq, yp, &xp, &ind->extraDoseNewXout, &gitol, &(op->RTOL), &(op->ATOL), &gitask,
                              &istate, &giopt, rwork, &lrw, iwork, &liw, jdum, &jt);
@@ -1685,7 +1685,7 @@ extern "C" void ind_dop0(rx_solve *rx, rx_solving_options *op, int solveid, int 
         // Bad Solve => NA
         badSolveExit(i);
       } else {
-        if (handleExtraDose(yp, xout, xp, &i, &istate, op, ind, u_inis, ctx)) {
+        if (handleExtraDose(yp, xout, xp, &i, op, ind)) {
           if (!isSameTimeDop(ind->extraDoseNewXout, xp)) {
             idid = dop853(neq,       /* dimension of the system <= UINT_MAX-1*/
                           c_dydt,       /* function computing the value of f(x,y) */
