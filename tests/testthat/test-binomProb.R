2d4deb7fd489da57d7d4db782dbe2a010eb5003b
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-10-04 11:57:51 -0500

    Test binomial CI for different ns

--- tests/testthat/test-binomProb.R
+++ tests/testthat/test-binomProb.R
@@ -41,4 +41,32 @@ test_that("test binomProb()", {
   expect_equal(binomProbs(x2,na.rm=TRUE), t1)
   expect_equal(binomProbs(x2, onlyProbs=FALSE, na.rm=TRUE), t2)
 
+
+  n <- 50
+  z <- qnorm(1 - 0.025)
+  z2 <- z * z
+  c1 <- 2*n*m + z2
+  c2 <- z*sqrt(z2 - 1.0/n + 4*n*v + (4.0*m-2.0))+1.0
+  c3 <- 2*(n+z2)
+  z2.5 <- (c1 + c(-1, 1) * c2) / c3
+
+  n <- 50
+  z <- qnorm(1 - 0.05)
+  z2 <- z * z
+  c1 <- 2*n*m + z2
+  c2 <- z*sqrt(z2 - 1.0/n + 4*n*v + (4.0*m-2.0))+1.0
+  c3 <- 2*(n+z2)
+  z5 <- (c1 + c(-1, 1) * c2) / c3
+
+
+  t1 <- c("2.5%"=z2.5[1],
+          "5%"=z5[1],
+          "50%"=m,
+          "95%"=z5[2],
+          "97.5%"=z2.5[2])
+
+  expect_equal(binomProbs(x, n=50), t1)
+
+ v ## now get test binomial predictions
+
 })
