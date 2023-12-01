ccb4ff54b35f19b43ca3ef39d9d78aac1fc12062
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-12-01 17:41:07 -0600

    Add case for etas in second but not first

--- tests/testthat/test-ui-mod-functions.R
+++ tests/testthat/test-ui-mod-functions.R
@@ -360,6 +360,61 @@ rxTest({
 
     expect_equal(m1$theta,
                  c(tka = 0.45, tcl = 1, tv = 3.45, add.sd = 0.7, tkin = 0, tkout = 0, tic50 = 2.30258509299405, gamma = 1, idr.sd = 1))
+
+  })
+
+  test_that("etas in second model but not first", {
+
+    ocmt <- function() {
+      ini({
+        tka <- exp(0.45)
+        tcl <- exp(1)
+        tv <- exp(3.45)
+        add.sd <- 0.7
+      })
+      model({
+        ka <- tka
+        cl <- tcl
+        v <- tv
+        d/dt(depot) = -ka * depot
+        d/dt(center) = ka * depot - cl / v * center
+        cp = center / v
+        cp ~ add(add.sd)
+      })
+    }
+
+    idr <- function() {
+      ini({
+        tkin <- log(1)
+        tkout <- log(1)
+        tic50 <- log(10)
+        gamma <- fix(1)
+        idr.sd <- 1
+        eta.kin ~ 0.1
+        eta.kout ~ 0.1
+        eta.ic50 ~ 0.1
+      })
+      model({
+        kin <- exp(tkin + eta.kin)
+        kout <- exp(tkout + eta.kout)
+        ic50 <- exp(tic50 + eta.ic50)
+        d/dt(eff) <- kin - kout*(1-ceff^gamma/(ic50^gamma+ceff^gamma))
+        eff ~ add(idr.sd)
+      })
+    }
+
+    m1 <- rxAppendModel(ocmt %>% model(ceff=cp,append=TRUE), idr)
+
+    expect_equal(m1$omega,
+                 lotri({
+                   eta.kin ~ 0.1
+                   eta.kout ~ 0.1
+                   eta.ic50 ~ 0.1
+                 }))
+
+    expect_equal(m1$theta,
+                 c(tka = 1.56831218549017, tcl = 2.71828182845905, tv = 31.5003923087479, add.sd = 0.7, tkin = 0, tkout = 0, tic50 = 2.30258509299405, gamma = 1, idr.sd = 1))
+
   })
 
 
