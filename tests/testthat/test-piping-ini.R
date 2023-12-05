819476deed4a177d7f484bf6c45ecc8bdc9cb0c4
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-12-05 15:50:31 -0600

    Test piping ini

--- tests/testthat/test-piping-ini.R
+++ tests/testthat/test-piping-ini.R
@@ -16,6 +16,25 @@ test_that("piping with ini can update labels (rxode2/issues#351)", {
   expect_equal(newLabelUi$iniDf$label[newLabelUi$iniDf$name == "a"], "bar")
 })
 
+test_that("piping with ini can remove labels (#627)", {
+
+  mod <- function() {
+    ini({
+      a <- 1
+      label("foo")
+      addSd <- 2
+    })
+    model({
+      b <- a
+      b ~ add(addSd)
+    })
+  }
+  ui <- rxode2(mod)
+  expect_equal(ui$iniDf$label[ui$iniDf$name == "a"], "foo")
+  newLabelUi <- ini(ui, a = label(NULL))
+  expect_equal(ui$iniDf$label[ui$iniDf$name == "a"], NA_character_)
+})
+
 test_that("piping with ini gives an error pointing the user to use label for character rhs (rxode2/issues#351)", {
   mod <- function() {
     ini({
