18849911190319b7dc22f869a6df4285483abfb8
Matthew Fidler <matthew.fidler@gmail.com>
2024-08-28 15:40:59 -0500

    Change safeZero handling and add safePow and safeLog options

---
+++ tests/testthat/test-safeZero.R
@@ -0,0 +1,25 @@
+test_that("safeZero solving", {
+
+  m <- rxode2({
+    nan0 = 0/0
+    nanLog = log(-1)
+    nanPow = 0^-1
+    rPow = 1^-1.5
+  })
+
+  et <- et(1)
+
+  unsafe <- rxSolve(m, et, safeZero=FALSE, safePow=FALSE, safeLog=FALSE, useStdPow = TRUE)
+  expect_true(is.na(unsafe$nan0))
+  expect_true(is.na(unsafe$nanLog))
+  expect_false(is.finite(unsafe$nanPow))
+  expect_true(is.finite(unsafe$rPow))
+
+  safe <- rxSolve(m, et, safeZero=TRUE, safePow=TRUE, safeLog=TRUE, useStdPow=FALSE)
+
+  expect_false(is.na(safe$nan0))
+  expect_false(is.na(safe$nanLog))
+  expect_true(is.finite(safe$nanPow))
+  expect_true(is.finite(safe$rPow))
+
+})
