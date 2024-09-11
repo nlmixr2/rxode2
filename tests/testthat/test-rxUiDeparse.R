ef3fc43e6a672cd7af281ba836584b90dd82d22d
Matthew Fidler <matthew.fidler@gmail.com>
2024-09-11 06:33:11 -0500

    Add deparse testing

---
+++ tests/testthat/test-rxUiDeparse.R
@@ -0,0 +1,13 @@
+test_that("rxUiDeparse()", {
+
+  rxUiDeparse(rxControl(), "a")
+  expect_equal(rxUiDeparse(rxControl(), "a"),
+               str2lang("a <- rxControl()"))
+
+  expect_equal(rxUiDeparse(rxControl(covsInterpolation="linear", method="dop853",
+                                    naInterpolation="nocb", keepInterpolation="nocb", sigmaXform="variance",
+                                    omegaXform="variance", returnType="data.frame", sumType="fsum", prodType="logify",
+                                    sensType="central"), "a"),
+               str2lang("a <- rxControl(method = \"dop853\", covsInterpolation = \"linear\", returnType = \"data.frame\", sigmaXform = \"variance\", sumType = \"fsum\", prodType = c(\"long double\", \"double\", \"logify\"), sensType = \"central\", naInterpolation = \"nocb\", keepInterpolation = \"nocb\")"))
+
+})
