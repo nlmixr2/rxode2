575925e85fe0d1b3cad2db38b6a7671e98176c05
Matthew Fidler <matthew.fidler@gmail.com>
2025-01-26 18:31:00 -0600

    Add 3 compartment test

--- tests/testthat/test-solComp.R
+++ tests/testthat/test-solComp.R
@@ -184,6 +184,36 @@ if (requireNamespace("pmxTools", quietly = TRUE)) {
            })
          })
 
+  # 3 compartment tests
+  f <- function(dt,
+                V = 40, CL = 18, V2 = 297, Q = 10, Q2 = 7, V3 = 400,
+                DOSE=100) {
+    p1 <- CL
+    v1 <- V
+    p2 <- Q
+    p3 <- V2
+    p4 <- Q2
+    p5 <- V3
+    ka <- 0
+    alastNV <- c(DOSE, 0, 0)
+    rateNV <- c(0, 0)
+    oral0 <- 0
+    trans <- 1
+    ncmt <- 3
+    l <- .Call(`_rxode2_linCmtModelDouble`, dt, p1, v1, p2, p3, p4, p5, ka, alastNV, rateNV, ncmt, oral0, trans)$val
+    c(s=pmxTools::calc_sd_3cmt_linear_bolus(CL=CL, V=V, V2=V2, Q=Q,
+                                            V3=V3, Q3=Q2,
+                                            t=dt, dose=DOSE), l=l)
+  }
+
+  lapply(seq(.1, 10, by=0.1),
+         function(d) {
+           test_that(paste0("test the two compartment linear compartment solution at ", d), {
+             v <- f(d)
+             expect_equal(stats::setNames(v["s"], NULL),
+                          stats::setNames(v["l"], NULL))
+           })
+         })
 
 
 
