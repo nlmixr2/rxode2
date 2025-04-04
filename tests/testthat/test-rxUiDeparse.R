aa84a3130522314384a672a8f6fc942c8d0da506
Matthew Fidler <matthew.fidler@gmail.com>
2025-04-04 09:52:37 -0500

    Fix rxUiDeparse

--- tests/testthat/test-rxUiDeparse.R
+++ tests/testthat/test-rxUiDeparse.R
@@ -7,6 +7,6 @@ test_that("rxUiDeparse()", {
   expect_equal(rxUiDeparse(rxControl(covsInterpolation="linear", method="dop853",
                                     naInterpolation="nocb", keepInterpolation="nocb", sigmaXform="variance",
                                     omegaXform="variance", returnType="data.frame", sumType="fsum", prodType="logify"), "a"),
-               str2lang("a <- rxControl(method = \"dop853\", covsInterpolation = \"linear\", returnType = \"data.frame\", sigmaXform = \"variance\", sumType = \"fsum\" naInterpolation = \"nocb\", keepInterpolation = \"nocb\")"))
+               str2lang("a <- rxControl(method = \"dop853\", covsInterpolation = \"linear\", returnType = \"data.frame\", sigmaXform = \"variance\", sumType = \"fsum\", naInterpolation = \"nocb\", keepInterpolation = \"nocb\")"))
 
 })
