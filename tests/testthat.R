54d634ac8662e503a594c31245b8f76168b01bf3
Matthew L. Fidler <matthew.fidler@gmail.com>
2025-04-25 21:43:03 -0500

    Use location reporter to try to see what is going on for github runner.

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,9 +4,9 @@ setRxThreads(1L)
 library(data.table)
 setDTthreads(1L)
 ##test_check("rxode2", reporter = testthat::LocationReporter)
-test_check("rxode2")
+# test_check("rxode2")
 
-## test_check("rxode2",
-##   stop_on_failure = FALSE,
-##   reporter = testthat::LocationReporter
-## )
+test_check("rxode2",
+  stop_on_failure = FALSE,
+  reporter = testthat::LocationReporter
+)
