344bea99a6b2842d1ef86a8c81c1affeb5e6e358
Matthew Fidler <matthew.fidler@gmail.com>
2025-04-25 22:37:36 -0500

    only test rxode2

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,10 +3,10 @@ library(testthat)
 setRxThreads(1L)
 library(data.table)
 setDTthreads(1L)
-##test_check("rxode2", reporter = testthat::LocationReporter)
-# test_check("rxode2")
+## test_check("rxode2", reporter = testthat::LocationReporter)
+test_check("rxode2")
 
-test_check("rxode2",
-  stop_on_failure = FALSE,
-  reporter = testthat::LocationReporter
-)
+## test_check("rxode2",
+##   stop_on_failure = FALSE,
+##   reporter = testthat::LocationReporter
+## )
