6ecdd50fd24154b6f33c37547536ff5f6bc26c4b
Matthew Fidler <matthew.fidler@gmail.com>
2025-06-23 13:25:42 -0500

    Start adding variance for linearization

--- R/err.R
+++ R/err.R
@@ -17,6 +17,7 @@
   ## Available as external package http://ugrad.stat.ubc.ca/R/library/rmutil/html/BetaBinom.html
   ## "dbetabinomial", ## not in base R (but in glnmm2)
   "add" = 1,
+  "var" = 1,
   "norm" = 0,
   "dnorm" = 0,
   "prop" = 1,
@@ -253,7 +254,8 @@ rxPreferredDistributionName <- function(dist) {
   "pow", # 3
   "add + prop", # 4
   "add + pow", # 5
-  "none" # 6
+  "none", # 6
+  "var" #7
 )
 
 .rxDistributionType <- c(
