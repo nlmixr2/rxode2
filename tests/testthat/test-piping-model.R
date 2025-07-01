95e4607ebd0dd3835c3a6333b6b7de05421c14a0
Bill Denney <wdenney@humanpredictions.com>
2025-07-01 14:33:27 -0400

    Remove tests for dropping if blocks

--- tests/testthat/test-piping-model.R
+++ tests/testthat/test-piping-model.R
@@ -21,7 +21,6 @@ test_that(".isDropExpression", {
   expect_false(.isDropExpression(str2lang("lag(a) <- b")))
 
   # Test for if blocks
-  expect_true(.isDropExpression(str2lang("-if (.) .")))
   expect_false(.isDropExpression(str2lang("if (.) .")))
 })
 
@@ -135,23 +134,3 @@ test_that(".getModelineFromExpressionsAndOriginalLines", {
     3
   )
 })
-
-# test_that("model piping can change or remove an if block (#878)", {
-#   mod <- function() {
-#     ini({
-#       a <- 1
-#     })
-#     model({
-#       d <- a
-#       g <- a
-#       if (b > 1) {
-#         d <- 2
-#       }
-#     })
-#   }
-#   # modChangeD <- model(mod, d <- b)
-#   # expect_equal(modelExtract(modChangeD), c("d <- b", "g <- a", "if (b > 1) {     d <- b }"))
-#   # modNoD <- model(mod, -d)
-#   # expect_equal(modelExtract(modNoD), "g <- a")
-#   modNoIf <- model(mod, -if (.) .)
-# })
