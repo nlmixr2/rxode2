c0c8ce297413fea0b75deb40d12328e12d46270c
Matthew Fidler <matthew.fidler@gmail.com>
2025-07-10 20:58:05 -0500

    Add steady-state tests

--- tests/testthat/test-dur-0.R
+++ tests/testthat/test-dur-0.R
@@ -1,6 +1,6 @@
 rxTest({
 
-  test_that("dur=0 is treated as a bolus in non steady-state doses", {
+  test_that("dur=0 is treated as a bolus in non steady-state doses (ODE)", {
 
     f <- rxode2({
       cl <- 1.1
@@ -25,6 +25,77 @@ rxTest({
 
   })
 
+  test_that("dur=0 with f is treated the same as bolus with f (ODE)", {
+
+    f <- rxode2({
+      cl <- 1.1
+      v <- 20
+      d/dt(central) <- - (cl/v)*central
+      dur(central) <- dur2
+      f(central) <- fcentral
+      cp <- central/(v/1000)
+    })
+
+    e <- et(rate=-2, amt=100) %>%
+      seq(0, 10)
+
+    s <- rxSolve(f, e, c(dur2=0, fcentral=2))
+    sb <- rxSolve(f, e, c(dur2=0, fcentral=0.5))
+
+    e <- et(amt=100) %>%
+      seq(0, 10)
+
+    s2 <- rxSolve(f, e, c(dur2=100, fcentral=2))
+    s2b <- rxSolve(f, e, c(dur2=100, fcentral=0.5))
+
+    expect_equal(s$central, s2$central)
+    expect_equal(s$cp, s2$cp)
+
+  })
+
+  test_that("dur=0 is treated as a bolus in non steady-state doses (ODE, SS)", {
+
+    f <- rxode2({
+      cl <- 1.1
+      v <- 20
+      d/dt(central) <- - (cl/v)*central
+      dur(central) <- dur2
+      cp <- central/(v/1000)
+    })
+
+    e <- et(rate=-2, amt=100, ss=1, ii=12) %>%
+      seq(0, 10)
+
+    s <- rxSolve(f, e, c(dur2=0))
+
+    e <- et(amt=100, ss=1, ii=12) %>%
+      seq(0, 10)
+
+    s2 <- rxSolve(f, e, c(dur2=100))
+
+    expect_equal(s$central, s2$central)
+    expect_equal(s$cp, s2$cp)
+
+    f <- rxode2({
+      cl <- 1.1
+      v <- 20
+      d/dt(central) <- - (cl/v)*central
+      dur(central) <- dur2
+      lag(central) <- lag2
+      cp <- central/(v/1000)
+    })
+
+    e <- et(rate=-2, amt=100, ss=1, ii=12) %>%
+      seq(0, 10)
+
+    s <- rxSolve(f, e, c(dur2=0, lag2=2))
+
+    e <- et(amt=100, ss=1, ii=12) %>%
+      seq(0, 10)
+
+    s <- rxSolve(f, e, c(dur2=100, lag2=2))
+
+  })
 
 
 })
