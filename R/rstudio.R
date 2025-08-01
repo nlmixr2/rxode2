a713549454b82c61754cc13cd00a3cd1e143e69c
Matthew Fidler <matthew.fidler@gmail.com>
2025-08-01 07:23:44 -0500

    Remove check for rstudio version (and remove rstudioapi)

--- R/rstudio.R
+++ R/rstudio.R
@@ -1,26 +1,3 @@
-#' Get the version of RStudio running
-#'
-#' @return NULL if Rstudio is not running or rstudioapi isn't installed
-#' @export
-#' @keywords internal
-#' @author Matthew L. Fidler
-#' @examples
-#' .rstudio()
-.rstudio <- function() {
-  if (!requireNamespace("rstudioapi", quietly = TRUE)) {
-    return(numeric_version("0.0.0"))
-  } else {
-    .ret <- try(rstudioapi::getVersion(), silent=TRUE)
-    if (inherits(.ret, "try-error")) {
-      return(numeric_version("0.0.0"))
-    } else {
-      return(.ret)
-    }
-  }
-}
-
-.rstudioCompleteEnv <- new.env(parent = emptyenv())
-.rstudioCompleteEnv$evalComplete <- NULL
 #' Determine if Rstudio completion 2025+ is running
 #'
 #' @return boolean, is Rstudio completion 2025+ running
@@ -31,10 +8,6 @@
 #' # Determines if rstudio is running completion
 #' .rstudioComplete()
 .rstudioComplete <- function() {
-  if (is.null(.rstudioCompleteEnv$evalComplete)) {
-    .rstudioCompleteEnv$evalComplete <- .rstudio() >= numeric_version("2025.0.0")
-  }
-  if (!.rstudioCompleteEnv$evalComplete) return(FALSE)
   .sc <- try(sys.calls(), silent=TRUE)
   if (inherits(.sc, "try-error")) {
     return(FALSE)
