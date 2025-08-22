cc3e0a7ee7a522d102e063d1c0d93877d82ba4ca
Matthew Fidler <matthew.fidler@gmail.com>
2025-08-21 21:41:00 -0500

    mexpit and mlogit functions for mix()

--- R/mlogit.R
+++ R/mlogit.R
@@ -27,13 +27,18 @@
 #'   in the mix() estimation algorithm.
 #'
 #' @inheritParams rootSolve::multiroot
+#'
 #' @param returnRoot logical; If TRUE, return the root object,
-#'   otherwise return the root itself
+#'   otherwise return the root itself.
+#'
 #' @return A numeric vector of the log-scale numbers for use in
 #'   regressions where the sum of a set of probabilities must add to
 #'   be one.
+#'
 #' @export
+#'
 #' @author Matthew L. Fidler
+#'
 #' @examples
 #'
 #' mlogit(0.1, 0.2, 0.3)
@@ -52,25 +57,80 @@ mlogit <- function(..., maxiter = 10000, rtol = 1e-08, atol = 1e-10,
     stop("the sum of the probabilities must be less than 1",
          call. = FALSE)
   }
+  if (length(.n) == 1) {
+    return(logit(.n))
+  }
   f <- function(x) {
     .Call(`_rxode2_mlogit_f`, x, .n)
   }
   j <- function(x) {
     .Call(`_rxode2_mlogit_j`, x)
   }
-  .init <- logit(.n)
-  .ret <- rootSolve::multiroot(f = f, start = .init,
+  .init <- rep(0, length(.n))
+  .ret <- try(suppressWarnings(rootSolve::multiroot(f = f, start = .init,
                                jacfunc = j,
                                maxiter = maxiter, rtol = rtol, atol = atol,
-                               ctol = ctol)
+                               ctol = ctol)), silent = TRUE)
+  if (inherits(.ret, "try-error")) {
+    .init <- logit(.init)
+    .ret <- try(suppressWarnings(rootSolve::multiroot(f = f, start = .init,
+                                                      jacfunc = j,
+                                                      maxiter = maxiter, rtol = rtol, atol = atol,
+                                                      ctol = ctol)), silent = TRUE)
+    if (inherits(.ret, "try-error")) {
+      .init <- rep(3, length(.n))
+      .ret <- try(suppressWarnings(rootSolve::multiroot(f = f, start = .init,
+                                                        jacfunc = j,
+                                                        maxiter = maxiter, rtol = rtol, atol = atol,
+                                                        ctol = ctol)), silent = TRUE)
+      if (inherits(.ret, "try-error")) {
+        .init <- rep(-3, length(.n))
+        .ret <- try(suppressWarnings(rootSolve::multiroot(f = f, start = .init,
+                                                          jacfunc = j,
+                                                          maxiter = maxiter, rtol = rtol, atol = atol,
+                                                          ctol = ctol)), silent = TRUE)
+        if (inherits(.ret, "try-error")) {
+          stop("mlogit failed to find a solution.  Please check your input probabilities.",
+call. = FALSE)
+        }
+      }
+    }
+  }
   if (returnRoot) {
     .ret
   } else {
     .ret$root
   }
 }
-
+#' mexpit -- Convert log-scale numbers to probabilities
+#'
+#' This function converts log-scale numbers to probabilities.
+#'
+#' The probabilities are calculated using the following equation:
+#'
+#' \deqn{p_i = \frac{e^{x_i}}{1+\sum_{j=1}^{N-1} e^{x_j}}}
+#'
+#' This ensures one remaining probability will add to one, that is
+#'
+#' \deqn{p_N = \frac{1}{1+\sum_{j=1}^{N-1} e^{x_j}}}
+#'
+#'
+#' @param ...  numeric log-scale numbers to convert to probabilities.
+#' @return Probabilities that add up to a number less than 1.
+#' @export
+#' @author Matthew L. Fidler
+#' @examples
+#'
+#' m <- mlogit(0.1, 0.2, 0.3)
+#' mexpit(m)
+#'
+#' p <- mexpit(-3, 0.5, 3)
+#' mlogit(p)
+#'
 mexpit <- function(...) {
   .n <- as.numeric(unlist(list(...)))
+  if (length(.n) == 1) {
+    return(expit(.n))
+  }
   .Call(`_rxode2_mexpit`, .n)
 }
