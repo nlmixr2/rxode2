fb174a251a6402b7c46e56a443a487b2758c3ccb
Matthew Fidler <matthew.fidler@gmail.com>
2025-08-25 08:17:29 -0500

    Add test for working probit/probitInv

--- tests/testthat/test-probit.R
+++ tests/testthat/test-probit.R
@@ -7,4 +7,13 @@ rxTest({
     expect_error(rxode2("a=probitInv(b, c)"), NA)
     expect_error(rxode2("a=probitInv(b, c, d)"), NA)
   })
+
+  test_that("probitInv/probit work", {
+    p <- seq(0.01, 0.99, by = 0.01)
+    x <- qnorm(p)
+    f <- rxode2("a=probit(time);c=probitInv(a)")
+    s <- rxSolve(f, et(p))
+    expect_equal(x, s$a)
+    expect_equal(p, s$c)
+  })
 })
