e0e241c0a0299a42e3ede6d1f93a3745b2018bed
Matthew Fidler <matthew.fidler@gmail.com>
2025-12-04 18:02:06 -0600

    Use location reporter for more information in rhub

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,8 @@ library(testthat)
 setRxThreads(1L)
 library(data.table)
 setDTthreads(1L)
-## test_check("rxode2", reporter = testthat::LocationReporter)
-test_check("rxode2")
+test_check("rxode2", reporter = testthat::LocationReporter)
+#test_check("rxode2")
 
 ## test_check("rxode2",
 ##   stop_on_failure = FALSE,
