e809b01bae655f507e3dd8f0755157b8e79a3f94
Matthew Fidler <matthew.fidler@gmail.com>
2025-12-05 17:28:29 -0600

    Change to dontrun for now [no ci]

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,10 +3,6 @@ library(testthat)
 setRxThreads(1L)
 library(data.table)
 setDTthreads(1L)
-if (!identical(Sys.getenv("NOT_CRAN"), "true") &&
-      identical(Sys.info()["sysname"], "Darwin")) {
-  rxUnloadAll(set=FALSE)
-}
 test_check("rxode2", reporter = testthat::LocationReporter)
 #test_check("rxode2")
 
