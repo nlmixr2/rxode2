dbc7c548463aaec9baf752eb714487beb4839c0c
mattfidler <matthew.fidler@gmail.com>
2026-01-23 16:39:59 -0600

    Fix $eta to not have hash attached

--- R/ui.R
+++ R/ui.R
@@ -99,6 +99,10 @@
   # Save $model like nlmixr UI used to...
   .ret <- rxUiDecompress(.ret)
   assign("model", fun, envir=.ret)
+  if (exists("eta", .ret)) {
+    attr(.ret$eta, ".match.hash") <- NULL
+    #.ret$eta <- get("eta", .ret)
+  }
   rxUiCompress(.ret)
 }
 
