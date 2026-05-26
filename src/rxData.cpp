bbab7b1d721801b295d0182dee9ee9792d77934c
mattfidler <matthew.fidler@gmail.com>
2026-05-26 17:02:21 -0500

    Update methods

--- src/rxData.cpp
+++ src/rxData.cpp
@@ -5412,8 +5412,9 @@ SEXP rxSolveFromRaw_(const RObject &obj, const RObject &rawObj,
 }
 extern "C" int solveMethodThreadSafe(rx_solving_options* op) {
   int stiff = op->stiff;
-  // dop853, liblsoda, rkf78, rk4
-  return stiff == 2 || stiff == 0 || stiff == 5 || stiff == 6;
+  // dop853, liblsoda, rkf78, rk4, ck54
+  return stiff == 2 || stiff == 0 || stiff == 5 || stiff == 6 ||
+    stiff == 7;
 }
 
 
