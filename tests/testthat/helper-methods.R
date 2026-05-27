1c9d39ec0c3f0e780f66c96d061e636ba7fb0c5b
mattfidler <matthew.fidler@gmail.com>
2026-05-27 18:18:08 -0500

    Update pkgdown and dense output vignette

--- tests/testthat/helper-methods.R
+++ tests/testthat/helper-methods.R
@@ -2,11 +2,11 @@
 # general ode methods used
 .methods0 <- c("sem", "ros4", "bs", "dop5", "ab", "abm", "ck54", "rk4", "dop853", "rkf78", "liblsoda")
 #.methods0 <- c("ros4", "iem")
-.methods0 <- c("sb3a")
+#.methods0 <- c("sb3a")
+.methods0 <- c("dop853",  "liblsoda")
 # dense methods used for nmtest
 #.methods1 <- c("ddop853", "ddop5", "dbs", "dros4")
 # .methods1 <- c("dros4")
-.methods1 <- NULL
+.methods1 <- c("ddop853")
 # Linear compartment methods used for nmtest
-#.methods2 <- c("A", "B", "Ao", "Bo", "As", "Bs", "Ad", "Bd", "Al", "Bl")
-.methods2 <- NULL
+.methods2 <- c("A", "B", "Ao", "Bo", "As", "Bs", "Ad", "Bd", "Al", "Bl")
