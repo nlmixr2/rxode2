a57ca6aa1661274ee7956cd8b009c6bce12f3d7e
mattfidler <matthew.fidler@gmail.com>
2026-05-29 15:45:28 -0500

    Fix spelling

--- tests/testthat/helper-methods.R
+++ tests/testthat/helper-methods.R
@@ -1,7 +1,7 @@
 # Helper functions for the test cases of the methods.
 # general ode methods used
 .methods0 <- c("sdirk43","geng5", "radauiia5", "iiic6", "gauss6", "backwardEuler", "ros6", "ros43", "vern98", "dop87", "vern76", "vern65", "dop54", "rk43", "rkf32", "ssp3", "trapz", "sem", "ros4", "bs", "dop5", "ab", "abm", "ck54", "rk4", "dop853", "rkf78", "liblsoda", "cvode")
-.methods0 <- c("dop853", "libsoda")
+.methods0 <- c("dop853", "liblsoda")
 # dense methods used for nmtest
 .methods1 <- c("ddop853", "ddop5", "dbs", "dros4")
 .methods1 <- c("ddop853")
