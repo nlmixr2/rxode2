16394473a374a8e40b6bf546bae99596bad7a588
mattfidler <matthew.fidler@gmail.com>
2026-06-01 15:43:30 -0500

    Update f78

--- tests/testthat/test-rkf78.R
+++ tests/testthat/test-rkf78.R
@@ -1,11 +1,11 @@
 rxTest({
-  test_that("rkf78 integrates 1-compartment model correctly", {
+  test_that("f78 integrates 1-compartment model correctly", {
     mod <- rxode2::rxode2({
       d/dt(y) <- -y
     })
     et <- rxode2::eventTable()
     et$add.sampling(seq(0, 5, length.out=10))
-    out <- rxode2::rxSolve(mod, params=c(), events=et, inits=c(y=1), method="rkf78")
+    out <- rxode2::rxSolve(mod, params=c(), events=et, inits=c(y=1), method="yYmrrf78")
     expect_s3_class(out, "rxSolve")
     expect_equal(out$y, exp(-out$time), tolerance = 0.05)
   })
